e5c17e4c3a80659af4dde1a04ec4463d38140474
digitalEyes <digitaleyes00@gmail.com>
2014-07-11 09:50:47 -0400

    updates to layout

--- www/wp-content/themes/classtheme20140625/_layout.php
+++ www/wp-content/themes/classtheme20140625/_layout.php
@@ -30,7 +30,7 @@
 <span class="icon-bar"></span>
 <span class="icon-bar"></span>
 </button>
-<a class="navbar-brand" href="#">Janine's Place</a>
+<a id="main-title" class="navbar-brand" href="#">Janine's Place</a>
 </div>
 
 <!-- Collect the nav links, forms, and other content for toggling -->
